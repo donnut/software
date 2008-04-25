3300bdc76e0f6f5d5b3be3a637c5b500a5e5963e
Benno Schulenberg <benno@vertaalt.nl>
2008-04-25 13:00:07 +0000

    Textual tweak.

--- lib/localweb.py
+++ lib/localweb.py
@@ -57,7 +57,9 @@ class DomainIndex(htmlpage.Htmlpage):
               "coreutils, and bash.  After those: shared-mime-info, "
               "xdg-user-dirs, parts of libc and parts of util-linux-ng.  "
               "And then maybe aspell, dialog, diffutils, e2fsprogs, gawk, "
-              "kbd, make, psmisc, wget, and xkeyboard-config.</p><br>")
+              "kbd, make, psmisc, texinfo, wget, and xkeyboard-config.  "
+              "Most of the other domains are only interesting for people "
+              "who actually use those packages.</p><br>")
         write('  <table align=center border=2>\n'
               '   <tr align=left>\n'
               '    <th>Domain</th>\n'
