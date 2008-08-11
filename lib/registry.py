48341a920db9958e86121414ad7410d7c3fe7cb5
Benno Schulenberg <benno@vertaalt.nl>
2008-08-11 20:07:57 +0000

    Allowing a plus character ('+') in version numbers also during sorting.

--- lib/registry.py
+++ lib/registry.py
@@ -388,7 +388,7 @@ class Version:
             self.sort_key = major, minor, patch, 1, pretest
             return self.sort_key
         match = re.match(
-            r'([0-9]+)\.?([0-9]*)\.?([0-9]*)([a-zA-Z]?)$', self.name)
+            r'([0-9]+)\.?([0-9]*)\.?([0-9]*)([a-zA-Z+]?)$', self.name)
         if match:
             major = int(match.group(1))
             minor = patch = pretest = 0
@@ -404,7 +404,7 @@ class Version:
             self.sort_key = major, minor, patch, 0, pretest
             return self.sort_key
         match = re.match(
-            r'([0-9]+)\.?([0-9]*)\.?([0-9]*)[-.]?rel([0-9]+)$', self.name)
+            r'([0-9]+)\.?([0-9]*)\.?([0-9+]*)[-.]?rel([0-9]+)$', self.name)
         if match:
             major = int(match.group(1))
             minor = patch = pretest = 0
@@ -417,7 +417,7 @@ class Version:
             self.sort_key = major, minor, patch, 1, pretest
             return self.sort_key
         match = re.match(
-            r'([0-9]+)\.?([0-9]*)\.?([0-9]*)[-.]?(b|dev|pre|rc)([0-9]+)$',
+            r'([0-9]+)\.?([0-9]*)\.?([0-9+]*)[-.]?(b|dev|pre|rc)([0-9]+)$',
             self.name)
         if match:
             major = int(match.group(1))
